f687bcdb233055cc121b16098e13a4ebd6a1a088
Joan Lafuente <joan.lafuente.baeza@gmail.com>
2024-05-28 23:19:34 +0200

    Update plots

--- src/Utils/gen_plots_from_logs.py
+++ src/Utils/gen_plots_from_logs.py
@@ -4,9 +4,9 @@ import seaborn as sns
 
 # Code to generate plots of the loss from the logs of the training process
 
-name_model = "train_Medical_newdata_without_LoRA"
+name_model = "LoRA_MedicalReports_new_data"
 # Load the logs
-with open(f"/hhome/nlp2_g05/Asho_NLP/src/Logs/logs_{name_model}.json", "r") as f:
+with open(f"/hhome/nlp2_g05/MEDSUM/src/Logs/logs_{name_model}.json", "r") as f:
     training_logs = json.load(f)
 
 
@@ -20,5 +20,5 @@ eval_loss_values = [x['eval_loss'] for x in training_logs if 'eval_loss' in x.ke
 plt.figure(figsize=(15, 5))
 sns.lineplot(x=training_loss_steps, y=training_loss_values_batch, label="Training Loss")
 sns.lineplot(x=range(1, len(eval_loss_values)+1), y=eval_loss_values, label="Validation Loss")
-plt.savefig(f"/hhome/nlp2_g05/Asho_NLP/training_validation_loss{name_model}.png")
+plt.savefig(f"/hhome/nlp2_g05/MEDSUM/Training_plots/training_validation_loss{name_model}.png")
 plt.close()
